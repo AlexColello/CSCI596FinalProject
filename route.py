b72c563878fc8ba9b0858f84327f132c4663c0cb
Alex Colello <alexlcolello@gmail.com>
2021-12-15 12:17:06 -0800

    Reorganize to run multiple iterations in sync

--- route.py
+++ route.py
@@ -7,34 +7,64 @@ from pathlib import Path
 
 PARENT_DIR = Path(__file__).absolute().parent
 
-#num_cores = int(mp.cpu_count() / 2)
-num_cores = 1
-num_runs = num_cores
+num_iterations = 5
+num_cores = mp.cpu_count()
+# num_runs = num_cores
+num_runs = 2
 run_threads = int(num_cores/num_runs)
-num_iterations = 10
 
 print(num_cores, num_runs, run_threads)
 
 input_path = PARENT_DIR / 'test_data' / 'StickHub.dsn'
 output_folder = PARENT_DIR / 'output'
-jar_path = PARENT_DIR / 'freerouting-1.4.5.1.jar'
+jar_path = PARENT_DIR / 'freerouting-executable.jar'
 command_path = ['java', '-jar', str(jar_path)]
 
-def run_routing(num, iteration):
-    print(f'Starting run {num} iteration {iteration}')
-    run_folder = output_folder / f'run_{num}'
-    input_path = run_folder / f'iteration{iteration}.dsn'
-    output_path = run_folder / f'iteration{iteration+1}.dsn'
+def get_iteration_directory(iteration_num):
+    return output_folder / f'iter_{iteration_num}'
+
+def get_run_directory(iteration_num, run_num):
+    return get_iteration_directory(iteration_num) / f'run_{run_num}'
+
+def get_log_path(iteration, run_num):
+    return get_run_directory(iteration, run_num) / 'logs' / f'freerouter.log'
+
+def parse_results(iteration, run_num):
+    log_path = get_log_path(iteration, run_num)
+
+    with open(log_path, 'r') as f:
+        for line in f.readlines():
+            pass
+
+def init_iteration(num, input_dsn_path):
+    iteration_folder = get_iteration_directory(num)
+    os.makedirs(iteration_folder)
+    shutil.copyfile(input_dsn_path, iteration_folder / 'initial.dsn')
+
+    return iteration_folder
+
+def select_best_dsn(iteration_directory):
+    return iteration_directory / 'run_0' / 'output.dsn'
+
+def run_routing(iteration, run_num):
+    print(f'Starting run {run_num} iteration {iteration}')
+    iteration_folder = get_iteration_directory(iteration)
+    run_folder = get_run_directory(iteration, run_num)
+    os.mkdir(run_folder)
+    input_path = run_folder / 'input.dsn'
+    shutil.copyfile(iteration_folder / 'initial.dsn', input_path)
+    
+    output_path = run_folder / 'output.dsn'
     args = ['-de', input_path, '-do', output_path, '-mp', '1', '-mt', '1', '-test']
 
     process = Popen(command_path + args, cwd=run_folder)
     process.wait()
     
-    print(f'Finished run {num} iteration {iteration}')
+    print(f'Finished iteration {iteration} run {run_num}')
 
-    log_location = run_folder / 'logs' / 'freerouter.log'
-    new_log_path = run_folder / 'logs' / f'log{iteration}.log'
-    os.rename(log_location, new_log_path)
+    # original_log_location = run_folder / 'logs' / 'freerouter.log'
+    # log_path = get_log_path(run_num, iteration)
+    # os.rename(original_log_location, log_path)
 
     return output_path
 
@@ -42,24 +72,16 @@ def main():
 
     shutil.rmtree(output_folder, ignore_errors=True)
 
-    run_args = []
-    for run in range(num_runs):
-        run_folder = output_folder / f'run_{run}'
-        os.makedirs(run_folder)
-
-        new_input_path = run_folder / 'iteration0.dsn'
-        shutil.copyfile(input_path, new_input_path)
-
-        args = []
-        args.append(run)
-
-        run_args.append(args)
-
-    pool_size = int(num_cores/run_threads)
+    #pool_size = int(num_cores/run_threads)
+    pool_size = 2
+    previous_best_path = input_path
     with mp.Pool(pool_size) as p:
         for iteration in range(num_iterations):
-            routing_args = [x + [iteration] for x in run_args]
+            iteration_directory = init_iteration(iteration, previous_best_path)
+            routing_args = [(iteration, x) for x in range(num_runs)]
             print(p.starmap(run_routing, routing_args))
+
+            previous_best_path = select_best_dsn(iteration_directory)
 
 if __name__ == '__main__':
     main()
